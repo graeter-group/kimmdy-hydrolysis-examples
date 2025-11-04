3b472bcd7e350b3f9b2d01977c896544f41fbecb
Jannik Buhr <jannik.m.buhr@gmail.com>
2025-11-04 14:17:10 +0100

    plot forces

--- src/style.py
+++ src/style.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 from plotnine import theme_set, theme_matplotlib
-from kimmdy_paper_theme import plot_colors, auto_init, single_column, double_column, convert_to_rgb
+from kimmdy_paper_theme import (
+    plot_colors,
+    auto_init,
+    single_column,
+    double_column,
+    convert_to_rgb,
+)
 
 # https://www.nature.com/documents/NRJs-guide-to-preparing-final-artwork.pdf
 
@@ -22,20 +28,20 @@ rc["axes.ymargin"] = 0
 
 theme_set(theme_matplotlib(rc=rc))
 
-HITS_DARKBLUE = '#003063'
-HITS_SIGNALBLUE = '#004f9f'
-HITS_CYAN = '#0088c2'
-HITS_CYAN_LIGHT = '#55b4dc'
-HITS_GREEN = '#019050'
-HITS_GREEN_LIGHT = '#89b77a'
-HITS_MAGENTA = '#c3006b'
-HITS_MAGENTA_LIGHT = '#da7da6'
-HITS_YELLOW = '#ffcc00'
-HITS_YELLOW_LIGHT = '#ffe07d'
-MPI_GREEN = '#006c66'
-MPI_GREEN_SECONDARY = '#055'
-MPG_grey_dark = '#777777'
-experiment = '#0088c2'
-experiment_light = '#55b4dc'
-kimmdy = '#c3006b'
-kimmdy_light = '#da7da6'
+HITS_DARKBLUE = "#003063"
+HITS_SIGNALBLUE = "#004f9f"
+HITS_CYAN = "#0088c2"
+HITS_CYAN_LIGHT = "#55b4dc"
+HITS_GREEN = "#019050"
+HITS_GREEN_LIGHT = "#89b77a"
+HITS_MAGENTA = "#c3006b"
+HITS_MAGENTA_LIGHT = "#da7da6"
+HITS_YELLOW = "#ffcc00"
+HITS_YELLOW_LIGHT = "#ffe07d"
+MPI_GREEN = "#006c66"
+MPI_GREEN_SECONDARY = "#055"
+MPG_grey_dark = "#777777"
+experiment = "#0088c2"
+experiment_light = "#55b4dc"
+kimmdy = "#c3006b"
+kimmdy_light = "#da7da6"
